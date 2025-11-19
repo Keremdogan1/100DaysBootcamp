f3bbf5dcbcff9d402860da82fb3aeebcaa2609bf
Keremdogan1 <kerem.dogan111@gmail.com>
2025-11-19 15:36:21 +0300

    completed

---
+++ Day 6/Functions/reeborg.py
@@ -0,0 +1,25 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# counter = 0
+# while not at_goal():
+#     while (wall_on_right()):
+#         counter = 0
+#         if (wall_in_front()):
+#             turn_left()
+#         else:
+#             move()
+#
+#     if (wall_in_front):
+#         if (counter == 4):
+#             turn_left()
+#             counter = 0
+#         turn_right()
+#         if (not at_goal()):
+#             move()
+#         counter += 1
+#     else:
+#         turn_right()
