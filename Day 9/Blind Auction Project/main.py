6a8e277691ff3d5828b03611de4244f20f1a9e78
Keremdogan1 <kerem.dogan111@gmail.com>
2025-11-22 18:07:08 +0300

    completed

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,37 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+from art import logo
+import os
+print(logo)
+
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+secret_auction_list = []
+max = 0
+max_index = 0
+index = 0
+
+will_program_run = True
+
+while(will_program_run):
+    buyer = {
+        "name": "",
+        "bid": 0
+    }
+
+    buyer["name"] = input("What is your name? : ")
+    buyer["bid"] = int(input("What is your bid? : $"))
 
 
+    if(buyer["bid"] > max):
+        max = buyer["bid"]
+        max_index = index
+    secret_auction_list.append(buyer)
+
+    check = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if check == "yes":
+        will_program_run = True
+        index += 1
+        print("\n" *100)
+    else:
+        will_program_run = False
+
+print(f"The winner is {secret_auction_list[index]["name"]} with a bid of ${secret_auction_list[index]["bid"]}!")
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,30 @@
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+secret_auction_list = []
+max = 0
+max_index = 0
+index = 0
+
+will_program_run = True
+
+while(will_program_run):
+    buyer = []
+
+    name = input("What is your name? : ")
+    buyer.append(name)
+    bid = int(input("What is your bid? : $"))
+    buyer.append(bid)
+
+    if(buyer[1] > max):
+        max = buyer[1]
+        max_index = index
+    secret_auction_list.append(buyer)
+
+    check = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if check == "yes":
+        will_program_run = True
+        index += 1
+    else:
+        will_program_run = False
+
+print(f"The winner is {secret_auction_list[index][0]} with a bid of ${secret_auction_list[index][1]}!")
