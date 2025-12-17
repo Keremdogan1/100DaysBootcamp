735f1158d135d922408a208152535413c7a424f5
Keremdogan1 <kerem.dogan111@gmail.com>
2025-12-17 20:08:14 +0300

    updated

--- Day 32/Birthday Wisher Project/main.py
+++ Day 32/Birthday Wisher Project/main.py
@@ -36,19 +36,3 @@ for index, row in data.iterrows():
                 msg["To"] = email_address
 
                 connection.sendmail(email, email_address, msg.as_string())
-
-
-            print(content)
-
-        
-        
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
